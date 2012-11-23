b187ea3fb3964881e7cb229bed16976de9c7f2b7
David Pflug <david@tpflug.com>
2012-11-23 11:38:31 -0500

    Page 60

--- little_schemer.scm
+++ little_schemer.scm
@@ -43,3 +43,63 @@
    ((null? lat) '())
    ((eq? old (car lat)) (cons new (cdr lat)))
    (else (cons (car lat) (subst new old (cdr lat))))))
+
+(define (subst2 new o1 o2 lat)
+  (cond
+   ((null? lat) '())
+   ((or
+     (eq? o1 (car lat))
+     (eq? o2 (car lat)))
+    (cons new (cdr lat)))
+   (else (cons (car lat) (subst2 new o1 o2 (cdr lat))))))
+
+(define (multirember a lat)
+  (cond
+   ((null? lat) '())
+   (else
+    (cond
+     ((eq? a (car lat)) (multirember a (cdr lat)))
+     (else (cons (car lat) (multirember a (cdr lat))))))))
+
+(define (multiinsertR new old lat)
+  (cond
+   ((null? lat) '())
+   (else
+    (cond
+     ((eq? old (car lat)) (cons old (cons new (multiinsertR new old (cdr lat)))))
+     (else (cons (car lat) (multiinsertR new old (cdr lat))))))))
+
+(define (multiinsertL new old lat)
+  (cond
+   ((null? lat) '())
+   (else
+    (cond
+     ((eq? old (car lat)) (cons new (cons old (multiinsertL new old (cdr lat)))))
+     (else (cons (car lat) (multiinsertL new old (cdr lat))))))))
+
+(define (multisubst new old lat)
+  (cond
+   ((null? lat) '())
+   (else
+    (cond
+     ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
+     (else (cons (car lat) (multisubst new old (cdr lat))))))))
+
+(define (add1 n)
+  (+ n 1))
+
+(define (sub1 n)
+  (- n 1))
+
+(define (zero? n)
+  (eq? 0 n))
+
+(define (o+ n m)
+  (cond
+   ((zero? m) n)
+   (else (add1 (o+ n (sub1 m))))))
+
+(define (o- n m)
+  (cond 
+   ((zero? m) n)
+   (else (sub1 (o- n (sub1 m))))))
