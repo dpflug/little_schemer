71c72661bfdf1812310caff6fef9544d5fee2c5c
David Pflug <david@tpflug.com>
2012-11-22 19:54:18 -0500

    Page 48

--- little_schemer.scm
+++ little_schemer.scm
@@ -25,3 +25,21 @@
   (cond
    ((null? l) '())
    (else (cons (car (car l)) (firsts (cdr l))))))
+
+(define (insertR new old l)
+  (cond
+   ((null? l) '())
+   ((eq? old (car l)) (cons old (cons new (cdr l))))
+   (else (cons (car l) (insertR new old (cdr l))))))
+
+(define (insertL new old l)
+  (cond
+   ((null? l) '())
+   ((eq? old (car l)) (cons new l))
+   (else (cons (car l) (insertL new old (cdr l))))))
+
+(define (subst new old lat)
+  (cond
+   ((null? lat) '())
+   ((eq? old (car lat)) (cons new (cdr lat)))
+   (else (cons (car lat) (subst new old (cdr lat))))))
