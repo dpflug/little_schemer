71f2a69ba5a42e63eb268da714cd51e9e8b33526
David Pflug <david@tpflug.com>
2012-11-23 20:40:06 -0500

    Page 93b (here's where the real fun starts)

--- little_schemer.scm
+++ little_schemer.scm
@@ -270,16 +270,8 @@
   (cond
    ((and (null? l1) (null? l2)) #t)
    ((or (null? l1) (null? l2)) #f)
-   ((and (atom? (car l1))
-	 (atom? (car l2)))
-    (and
-     (eqan? (car l1) (car l2))
-     (eqlist? (cdr l1) (cdr l2))))
-   ((or (atom? (car l1))
-	(atom? (car l2)))
-    #f)
    (else
-    (and (eqlist? (car l1) (car l2))
+    (and (equal? (car l1) (car l2))
 	 (eqlist? (cdr l1) (cdr l2))))))
 
 (define (equal? s1 s2)
