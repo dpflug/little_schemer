42b1194fd9128e2784b060ecb02c68ab9288d460
David Pflug <david@tpflug.com>
2012-11-23 22:58:14 -0500

    p113b

--- little_schemer.scm
+++ little_schemer.scm
@@ -324,3 +324,9 @@
 	       (makeset
 		(multirember (car lat)
 			     (cdr lat)))))))
+
+(define (subset? set1 set2)
+  (cond
+   ((null? set1) #t)
+   ((member? (car set1) set2) (subset? (cdr set1) set2))
+   (else #f)))
